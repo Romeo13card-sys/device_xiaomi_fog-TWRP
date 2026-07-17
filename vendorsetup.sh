d5eb0d29b87d96d9dff22a9d0abb27918f8d1b6b
Romeo13card-sys <laschevskiydaniil@rambler.ru>
2026-07-17 19:08:33 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -51,7 +51,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export FOX_BUILD_TYPE="Stable"
     export FOX_VARIANT="default"
-	export FOX_MAINTAINER_PATCH_VERSION="04"
 	export OF_MAINTAINER="romeo_13card"
         export FOX_VIRTUAL_AB_DEVICE=1
         export OF_ADVANCED_SECURITY=1
